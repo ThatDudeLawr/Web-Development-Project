8729183898c4334461a266ef15684eebba0d692d
Laurentiu-Stefan Mures <32162117+ThatDudeLawr@users.noreply.github.com>
2020-03-02 00:21:57 +0200

    added some comments

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,12 +1,14 @@
 var coll = document.getElementsByClassName("dropdown");
 var i;
 
+//Iterates trough all the elements inside the Dropdown parent
 for (i = 0; i < coll.length; i++) 
 {
   if(coll[i].addEventListener)
   coll[i].addEventListener("click", function(){expand_dropdown(this);},true);
 }
 
+//Checks if the child height is "max" and expands it if not
 function expand_dropdown(a)
 {
   a.classList.toggle("active");
